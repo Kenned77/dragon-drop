0eb8d733e4ca9ba2180f8dd27afd8a915a82b182
Kenneth Bidstrup <kenneth.bidstrup@gmail.com>
2019-01-21 15:09:06 -0800

    Print board

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,2 +1,24 @@
+window.onload = function() {
+  tableCells = [];
+  startGame();
+}
 
-document.write('testing');
+function startGame()  {
+  gameBoard = new Board();
+  $("#board").append(gameBoard.element);
+}
+
+function Board()  {
+  this.size = 10;
+  this.element = $("<table>");
+  for(var i = 0; i < this.size; i++)  {
+    tableCells[i] = [];
+    var nextRow = $("<tr>");
+    this.element.append(nextRow);
+    for(var j = 0; j < this.size; j++) {
+      var nextColumn = $("<td>");
+      nextRow.append(nextColumn);
+      tableCells[i][j] = nextColumn;
+    }
+  }
+}
