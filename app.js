070a74dd692888dd19fae5b1c20100e14980197e
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-21 12:05:58 -0800

    Use ejs; move index file to views directory

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
+app.set('view engine', 'ejs');
+
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
