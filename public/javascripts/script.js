3fb54b0c3e82f183c30126967f63606f05460704
Kenneth Bidstrup <kenneth.bidstrup@gmail.com>
2019-01-21 16:51:48 -0800

    Add id to cells

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -11,14 +11,17 @@ function startGame()  {
 function Board()  {
   this.size = 10;
   this.element = $("<table>");
+  this.id = 1;
   for(var i = 0; i < this.size; i++)  {
     tableCells[i] = [];
     var nextRow = $("<tr>");
     this.element.append(nextRow);
     for(var j = 0; j < this.size; j++) {
-      var nextColumn = $("<td>");
+      // var nextColumn = $("<td>");
+      var nextColumn = $("<td>").attr( 'id', 'cell-' + this.id);
       nextRow.append(nextColumn);
       tableCells[i][j] = nextColumn;
+      this.id++;
     }
   }
 }
