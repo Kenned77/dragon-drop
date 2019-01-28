89bf28a0f17245e057359e61d9ee63d853389bc8
Kenneth Bidstrup <kenneth.bidstrup@gmail.com>
2019-01-28 14:29:54 -0800

    add if check to style and place all L shape piece small and larges

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -67,7 +67,51 @@ function placePiece() {
   } else if (type === 'rectangle-5-horizontal') {
     pieceElligalMove = [7, 8, 9, 10, 17, 18, 19, 20, 27, 28, 29, 30, 37, 38, 39, 40, 47, 48, 49, 50, 57, 58, 59, 60, 67, 68, 69, 70, 77, 78, 79, 80, 87, 88, 89, 90, 97];
     piece = [0, 1, 2, 3, 4];
-  } else {
+  } else if(type === 'l-shape-small-1') {
+    pieceElligalMove = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+    piece = [0, 10, 11];
+    // *
+    // **
+  } else if(type === 'l-shape-small-2') {
+    pieceElligalMove = [1, 11, 21, 31, 41, 51, 61, 71, 81, 91];
+    piece = [0, 9, 10];
+    //  *
+    // **
+  } else if(type === 'l-shape-small-3') {
+    pieceElligalMove = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+    piece = [0, 1, 11];
+    // **
+    //  *
+  } else if(type === 'l-shape-small-4') {
+    pieceElligalMove = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+    piece = [0, 1, 10];
+    // **
+    // *
+  } else if(type === 'l-shape-large-1') {
+    pieceElligalMove = [9, 10, 19, 20, 29, 30, 39, 40, 49, 50, 59, 60, 69, 70, 79];
+    piece = [0, 10, 20, 21, 22];
+    // *
+    // *
+    // ***
+  } else if(type === 'l-shape-large-2') {
+    pieceElligalMove = [1, 2, 11, 12, 21, 22, 31, 32, 41, 42, 51, 52, 61, 62, 71, 72, 81];
+    piece = [0, 10, 20, 19, 18];
+    //   *
+    //   *
+    // ***
+  } else if(type === 'l-shape-large-3') {
+    pieceElligalMove = [9, 10, 19, 20, 29, 30, 39, 40, 49, 50, 59, 60, 69, 70, 79];
+    piece = [0, 1, 2, 12, 22];
+    // ***
+    //   *
+    //   *
+  } else if(type === 'l-shape-large-4') {
+    pieceElligalMove = [9, 10, 19, 20, 29, 30, 39, 40, 49, 50, 59, 60, 69, 70, 79];
+    piece = [0, 1, 2, 10, 20];
+    // ***
+    // *
+    // *
+  } else  {
     console.log('not quite yet');
   }
 
