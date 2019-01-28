71003ef80730bdd36298797847fea12a75cbf0b6
Kenneth Bidstrup <kenneth.bidstrup@gmail.com>
2019-01-28 13:40:16 -0800

    add if check to style and place all square pieces

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -17,12 +17,69 @@ var Board = function() {
     tableCells[i] = [];
     var nextRow = $("<tr>");
     for(var j = 0; j < this.size; j++) {
-      var nextColumn = $("<td>").attr( 'id', 'cell-' + this.cellId);
+      var nextColumn = $("<td>" + this.cellId + "</td>").attr( 'id', 'cell-' + this.cellId);
       tableCells[i][j] = nextColumn;
       this.cellId++;
       nextRow.append(nextColumn);
     }
     this.element.append(nextRow);
+  }
+}
+
+
+function placePiece() {
+  var type, startNum, pieceElligalMove, piece, inFrame
+  type = document.getElementById("type").value;
+  startNum = parseInt(document.getElementById("startNum").value);
+  inFrame = true;
+
+  if (type === 'square-small') {
+    pieceElligalMove = [101];
+    piece = [0];
+  } else if (type === 'square-medium') {
+    pieceElligalMove = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+    piece = [0, 1, 10, 11];
+  } else if (type === 'square-large') {
+    pieceElligalMove = [9, 10, 19, 20, 29, 30, 39, 40, 49, 50, 59, 60, 69, 79];
+    piece = [0, 1, 2, 10, 11, 12, 20, 21, 22];
+  } else if (type === 'rectangle-2-vertical') {
+    pieceElligalMove = [91];
+    piece = [0, 10];
+  } else if (type === 'rectangle-2-horizontal') {
+    pieceElligalMove = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+    piece = [0, 1];
+  } else if (type === 'rectangle-3-vertical') {
+    pieceElligalMove = [81];
+    piece = [0, 10, 20];
+  } else if (type === 'rectangle-3-horizontal') {
+    pieceElligalMove = [9, 10, 19, 20, 29, 30, 39, 40, 49, 50, 59, 60, 69, 70, 79, 80, 89, 90, 99];
+    piece = [0, 1, 2];
+  } else {
+    console.log('not quite yet');
+  }
+
+  // console.log(startNum);
+
+  if (startNum >= pieceElligalMove[pieceElligalMove.length - 1]) {
+      inFrame = false;
+      console.log('nope cant do!');
+  } else {
+    for ( var i = 0; i < pieceElligalMove.length; i++ ) {
+      if (startNum === pieceElligalMove[i]) {
+        inFrame = false;
+        console.log('nope cant do!');
+      }
+    }
+  }
+
+  for ( var i = 1; i <= 100; i++ ) {
+    $('#cell-' + i).css("background-color", 'transparent');
+  }
+
+  if (inFrame) {
+    for (var i = 0; i < piece.length; i++) {
+      $('#cell-' + (startNum + piece[i])).css("background-color", scuareColor);
+    }
   }
 }
 
