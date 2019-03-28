777d365ce36406396f154b5a1b79e5c5523186b6
Kenneth Bidstrup <kenneth.bidstrup@gmail.com>
2019-03-28 16:21:42 -0700

    Move functions to new file

---
+++ javascripts/dragPiece.js
@@ -0,0 +1,87 @@
+let boxCounter = 3;
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function place(type, startNum, isDrop) {
+  var inFrame, taken
+
+  inFrame = true;
+  taken = false;
+
+  if ( startNum >= pieces[type][0][pieces[type][0].length - 1] || startNum <= 0 ) {
+      inFrame = false;
+  } else {
+    for ( var i = 0; i < pieces[type][0].length; i++ ) {
+      if (startNum === pieces[type][0][i]) {
+        inFrame = false;
+      }
+    }
+  }
+
+  if (inFrame) {
+    if (pieceCanFit(type, startNum)) {
+      for (var i = 0; i < pieces[type][1].length; i++) {
+        if (isDrop) {
+          $('#' + (startNum + pieces[type][1][i])).removeClass('no-color').addClass('color');
+        } else {
+          $('#' + (startNum + pieces[type][1][i])).addClass('backgroundShadow');
+        }
+      }
+
+      if (startNum > 0) {
+        if (isDrop) {
+          score += pieces[type][1].length;
+          displayScore(score);
+        }
+      }
+    }
+  }
+
+  // Create empty 10 * 10 empty array
+  createArr(arr);
+
+  // Create a copy of the grid based on which og the cells have a class of color
+  dublicateGridToArr(arr);
+  checkAndRemove();
+}
+
+function drop(ev) {
+  removeBackgroundShadow();
+
+  let correct = true;
+  let startNum;
+
+  ev.preventDefault();
+
+  startNum = parseInt(ev.target.id);
+  startNum = startNum - coordinatRow;
+  startNum = startNum - coordinatCol;
+
+  // Check if move is allowed
+  for (var i = 0; i < pieces[type][1].length; i++) {
+    if ($('#' + (startNum + pieces[type][1][i])).hasClass('color')) {
+      correct = false;
+    }
+  }
+
+  if (boxCounter >= 0 && correct) {
+    $('#piece-' + hide).html('');
+    boxCounter--;
+  }
+
+  if (boxCounter === 0) {
+    createPieces();
+    boxCounter = 3;
+    checkingAnyMoveLeft();
+  }
+
+  place(type, startNum, true);
+
+  checkingAnyMoveLeft();
+
+  for ( let i = 1; i <= 3; i++ ) {
+    $('#tableId-' + i).removeClass('pieceSizeDraged').addClass('pieceSize');
+  }
+}
--- javascripts/script.js
+++ javascripts/script.js
@@ -97,10 +97,6 @@ let Piece = function(size, cellId, type) {
   newPiece = [];
 }
 
-let allowDrop = (ev) => {
-  ev.preventDefault();
-}
-
 let hide;
 let type;
 
@@ -113,52 +109,6 @@ let drag = (ev) => {
 
   $(ev.target).removeClass('pieceSize').addClass('pieceSizeDraged');
   shouldStartNewGameButtonHide(true);
-}
-
-let boxCounter = 3;
-
-let drop = (ev) => {
-
-  removeBackgroundShadow();
-
-  let correct = true;
-  let startNum;
-
-  ev.preventDefault();
-
-  startNum = parseInt(ev.target.id)
-
-  startNum = startNum - coordinatRow;
-  startNum = startNum - coordinatCol;
-
-  // var data = ev.dataTransfer.getData("text");
-
-
-  // Check if move is allowed
-  for (var i = 0; i < pieces[type][1].length; i++) {
-    if ($('#' + (startNum + pieces[type][1][i])).hasClass('color')) {
-      correct = false;
-    }
-  }
-
-  if (boxCounter >= 0 && correct) {
-    $('#piece-' + hide).html('');
-    boxCounter--;
-  }
-
-  if (boxCounter === 0) {
-    createPieces();
-    boxCounter = 3;
-    checkingAnyMoveLeft();
-  }
-
-  place(type, startNum, true);
-
-  checkingAnyMoveLeft();
-
-  for ( let i = 1; i <= 3; i++ ) {
-    $('#tableId-' + i).removeClass('pieceSizeDraged').addClass('pieceSize');
-  }
 }
 
 let checkingAnyMoveLeft = () => {
@@ -261,49 +211,6 @@ let removeTiles = (direction, row, col) => {
       removeTiles(direction, row + 10, col);
     }
   }, 50);
-}
-
-let place = (type, startNum, isDrop) => {
-  var inFrame, taken
-
-  inFrame = true;
-  taken = false;
-
-  if ( startNum >= pieces[type][0][pieces[type][0].length - 1] || startNum <= 0 ) {
-      inFrame = false;
-  } else {
-    for ( var i = 0; i < pieces[type][0].length; i++ ) {
-      if (startNum === pieces[type][0][i]) {
-        inFrame = false;
-      }
-    }
-  }
-
-  if (inFrame) {
-    if (pieceCanFit(type, startNum)) {
-      for (var i = 0; i < pieces[type][1].length; i++) {
-        if (isDrop) {
-          $('#' + (startNum + pieces[type][1][i])).removeClass('no-color').addClass('color');
-        } else {
-          $('#' + (startNum + pieces[type][1][i])).addClass('backgroundShadow');
-        }
-      }
-
-      if (startNum > 0) {
-        if (isDrop) {
-          score += pieces[type][1].length;
-          displayScore(score);
-        }
-      }
-    }
-  }
-
-  // Create empty 10 * 10 empty array
-  createArr(arr);
-
-  // Create a copy of the grid based on which og the cells have a class of color
-  dublicateGridToArr(arr);
-  checkAndRemove();
 }
 
 let displayScore = (sc, row, col) => {
